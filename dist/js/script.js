369e1406b4897ab3fff9bc4b0fcd749213d4b2c3
Artyom <artematazhetdinov4@gmail.com>
2022-06-17 21:14:20 +0300

    end desctop version

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    new ChiefSlider('.reviews__carousel');
-  });
 
+  var slider = tns({
+    container: '.reviews__carousel',
+    items: 2,
+    nav: false,
+    slideBy: 'page',
+    autoplay: false,
 
+    controlsText:['<img   class = "next" src = "../icons/left.png">', '<img <img class = "prev"  src = "../icons/right.png">']
+    });   
+    
+  var slider = tns({
+    container: '.client__wrapper',
+    items: 5,
+    nav: false,
+    slideBy: 'page',
+    autoplay: false,
+    controlsText:['<img   class = "next" src = "../icons/left.png">', '<img <img class = "prev"  src = "../icons/right.png">']
+    }); 
